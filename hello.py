7439a8a76185445d63537b086098fa5f8e3a0b05
Taimoor Anwar <77386699+taimoor889@users.noreply.github.com>
2024-12-22 14:25:34 +0100

    Add hello.py file

---
+++ hello.py
@@ -0,0 +1,20 @@
+print("hiiii guys")   
+
+
+print("Hello World")
+f=10
+def addition(*args):
+    print(args)
+    print(type(args))
+     
+
+addition(10,20,30)
+
+def subtration(a,b):
+    return a-b 
+print(subtration(20,10))
+
+subtinlambda=lambda a,b:a-b 
+print(subtinlambda(40-10))
+
+
